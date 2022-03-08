859f50601462dc8a7148a822f4fa2e9598c85ab1
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-08 19:11:58 +0000

    Add ArrayBiDim project

---
+++ ArrayBiDim/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayBiDim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for array horizontal values
+            Console.Write("Array horizontal dimensions: ");
+
+            // Ask user for array vertical values
+            Console.Write("Array vertical dimensions: ");
+        }
+    }
+}
