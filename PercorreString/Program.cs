5c1ce16a7d10f9beb28d47af48292bcb2655788e
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-08 11:16:54 +0000

    Print user input string without user input char

--- PercorreString/Program.cs
+++ PercorreString/Program.cs
@@ -6,10 +6,33 @@ namespace PercorreString
     {
         static void Main(string[] args)
         {
-            // Ask user for string
-            Console.WriteLine("Please write a string");
+            // The user input string and char
+            string s, sC;
+            char c ;
 
+            // Ask user for string
+            Console.Write("Please write a string: ");
+
+            // Store user input in string
+            s = Console.ReadLine();
+
+            // Ask user for char
+            Console.Write("Please write a character: ");
+
+            // Store user input in char
+            sC = Console.ReadLine();
+
+            // Convert char in string to char variable
+            c = sC[0];
             
+            Console.Write($"'{s}' without '{c}': ");
+
+            // FOR loop prints string char by char, whitout user input char
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != c)
+                    Console.Write(s[i]);
+            }
         }
     }
 }
