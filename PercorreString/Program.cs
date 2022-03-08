496e2ac26a4841abf6a9c9df7314b7c2617c3ef0
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-08 10:59:52 +0000

    Add PercorreString project

---
+++ PercorreString/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for string
+            Console.WriteLine("Please write a string");
+
+            
+        }
+    }
+}
