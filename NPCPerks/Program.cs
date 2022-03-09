d24d393922a51ce9699ec75a3afbb992a785baba
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-03-09 15:41:08 +0000

    Add NPCPerks project

---
+++ NPCPerks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NPCPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask user for number of NPCs
+            Console.Write("Insert the number of NPCs in your game: ");
+        }
+    }
+}
