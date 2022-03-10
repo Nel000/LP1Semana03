d1378e1abbcf7c1031c8a261dbff585c89fb820d
Manuel Geraldes <manuelgeraldes2000@gmail.com>
2022-03-10 15:43:20 +0000

    Improve comments

--- NPCPerks/Program.cs
+++ NPCPerks/Program.cs
@@ -12,20 +12,16 @@ namespace NPCPerks
             // The number of NPCs
             int npcNum;
 
-            // The array that stores each NPC
-            int[][] npc;
-
             // The amount of NPC traits
             int traits = 4;
 
-            // The value for each given NPC
-            //int npcVal = 0;
-
+            // The name of the current trait
             string traitName;
 
             // Defines when to stop attributing perks to an NPC
             bool hasEnded;
 
+            // Auxiliary variable
             int j;
 
             // Ask user for number of NPCs
@@ -33,7 +29,7 @@ namespace NPCPerks
 
             // Store user input in NPC number variable
             npcNum = Convert.ToInt32(Console.ReadLine());
-            npc = new int[npcNum][];
+
             // Assign number of NPCs to traits variable
             npcTraits = new NPCTraits[npcNum];
 
@@ -64,33 +60,41 @@ namespace NPCPerks
                     switch (traitName)
                     {
                         case "Stealth":
+                            // Assign 'Stealth' trait to current NPC
                             npcTraits[i] |= NPCTraits.Stealth;
                             break;
                         case "Combat":
+                            // Assign 'Combat' trait to current NPC
                             npcTraits[i] |= NPCTraits.Combat;
                             break;
                         case "Lockpick":
+                            // Assign 'Lockpick' trait to current NPC
                             npcTraits[i] |= NPCTraits.Lockpick;
                             break;
                         case "Luck":
+                            // Assign 'Luck' trait to current NPC
                             npcTraits[i] |= NPCTraits.Luck;
                             break;
                         case "Stop":
+                            // Stop requesting traits for current NPC
                             hasEnded = true;
                             break;
                         default:
+                            // Retry getting trait for current NPC
                             Console.WriteLine("Not a valid trait");
                             j--;
                             break;
                     }
                     j++;
-                } 
+                }
+                // Stop when END condition is met or NPC already has max traits
                 while (!hasEnded || j < traits);
             }
 
-            // Goes through array of NPCs
+            // Go through array of NPCs
             for (int i = 0; i < npcNum; i++)
             {
+                // Details for current NPC
                 Console.WriteLine($"NPC {i + 1} has : {npcTraits[i]}");
 
                 // Check if current NPC has Combat and Luck perks
