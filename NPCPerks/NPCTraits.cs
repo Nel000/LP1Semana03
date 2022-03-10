b9728eecb50647e6dd1c0cbb1f7e1d2f4d6544fc
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-10 11:38:05 +0000

    Update enum with enumeration flags

--- NPCPerks/NPCTraits.cs
+++ NPCPerks/NPCTraits.cs
@@ -1,10 +1,13 @@
+using System;
+
 namespace NPCPerks
 {
+    [Flags]
     public enum NPCTraits
     {
-        Stealth = 1,
-        Combat,
-        Lockpick,
-        Luck
+        Stealth = 1 << 0,   // 1
+        Combat = 1 << 1,    // 2
+        Lockpick = 1 << 2,  // 4
+        Luck = 1 << 3       // 8
     }
 }
--- NPCPerks/Program.cs
+++ NPCPerks/Program.cs
@@ -6,14 +6,21 @@ namespace NPCPerks
     {
         static void Main(string[] args)
         {
+            // Reference to NPC traits enum
+            NPCTraits npcTraits;
+
             // The number of NPCs
             int npcNum;
+
+            // The array that stores each NPC
+            int[][] npc;
 
             // Ask user for number of NPCs
             Console.Write("Insert the number of NPCs in your game: ");
 
             // Store user input in NPC number variable
             npcNum = Convert.ToInt32(Console.ReadLine());
+            npc = new int[npcNum][];
 
             // Request traits for each NPC
             Console.WriteLine("For each NPC, insert up to 4 traits:");
@@ -24,7 +31,17 @@ namespace NPCPerks
             Console.WriteLine("Lockpick (3)");
             Console.WriteLine("Luck     (4)");
 
-            // FOR loop to the amount given by the userW
+            // FOR loop to the amount given by the user
+            for (int i = 0; i < npcNum; i++)
+            {
+                Console.Write($"NPC {i}: ");
+                for (int j = 0; j <= 4; j++)
+                {   
+                    
+                }
+            }
+
+            // Goes through array of NPCs
         }
     }
 }
